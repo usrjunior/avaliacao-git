06e26b6c3ae00b530913cfcaac45adbdd1740a45
Junior <Junior@ufpe.br>
2013-03-15 11:01:20 -0300

    commit alteracao no eclipse

--- src/test/projecteuler/Problems.java
+++ src/test/projecteuler/Problems.java
@@ -4,6 +4,9 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import projecteuler.problems.Problem1;
+import projecteuler.problems.Problem2;
+
 /**
  * 
  * @author Tarcisio
@@ -15,13 +18,13 @@ public class Problems {
 
 	@Test
 	public void testProblem1() {
-		Problem p = new Problem1();
+		Problem1 p = new Problem1();
 		assertEquals(233168, p.solve());
 	}
 
 	@Test
 	public void testProblem2() {
-		Problem p = new Problem2();
+		Problem2 p = new Problem2();
 		assertEquals(4613732, p.solve());
 	}
 
